671b9418a54392f3fc36fa6d360ae28534c011a6
joseph-allen <joseph.allen.1994@hotmail.com>
2018-09-14 18:04:14 +0100

    High scores

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -8,6 +8,12 @@ class Skills extends Component {
     return (
       <div className="skills">
         <h2>SKILLS</h2>
+        <ul>
+          <li>1. Fr0nt-3nd ............ 1200</li>
+          <li>2. Data Sc1ence .......... 900</li>
+          <li>3. Pr0j3ct Management .... 400</li>
+          <li>4. Developm3nt ........... 300</li>
+        </ul>
       </div>
     );
   }
